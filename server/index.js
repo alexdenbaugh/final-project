9b1d67328763007de85a2bc0cdc52924ad4b277c
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-05-25 16:19:29 -0700

    Update server/index.js to handle post requests to add new game posts to the database. #1

--- server/index.js
+++ server/index.js
@@ -2,15 +2,62 @@ require('dotenv/config');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
+const ClientError = require('./client-error');
+const pg = require('pg');
 
 const app = express();
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const jsonMiddleware = express.json();
 app.use(jsonMiddleware);
 
-app.post('./boardGamePosts', (req, res, next) => {
-  // eslint-disable-next-line no-console
-  console.log(req.body);
+app.post('/boardGamePosts', (req, res, next) => {
+  const { lender, game, gameId, gameImg, comments } = req.body;
+  if (!lender || !game || !gameId || !gameImg || !comments) {
+    throw new ClientError(400, 'lender, game, gameId, gameImg, and comments are required fields');
+  }
+  const sql = `
+    insert into "posts" ("lenderName", "gameName", "gameApiId", "gameThumbNail", "lenderComments")
+    values ($1, $2, $3, $4, $5)
+    returning *;
+  `;
+  const params = [lender, game, gameId, gameImg, comments];
+  db.query(sql, params)
+    .then(result => {
+      const [post] = result.rows;
+      res.status(201).send(post);
+    })
+    .catch(err => next(err));
+
+  // console.log('sql:', sql, 'params', params);
+});
+
+app.delete('/boardGamePosts/:postId', (req, res, next) => {
+  const postId = parseInt(req.params.postId, 10);
+  if (!Number.isInteger(postId) || postId < 1) {
+    throw new ClientError(400, 'post must be a positive integer');
+  }
+  const sql = `
+    delete from "posts"
+    where "postId" = $1
+    returning *
+  `;
+  const params = [postId];
+  db.query(sql, params)
+    .then(result => {
+      const [removed] = result.rows;
+      if (!removed) {
+        throw new ClientError(404, `Could not find postId ${postId}`);
+      }
+      res.sendStatus(204);
+    })
+    .catch(err => next(err));
 });
 
 app.use(staticMiddleware);
