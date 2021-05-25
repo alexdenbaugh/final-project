23abae00a53ce81cf042512388adbf830ddd3084
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-05-25 13:33:22 -0700

    Added data structure to schema.sql along with some dummy data in database.sql. #1

---
+++ database/data.sql
@@ -0,0 +1,8 @@
+insert into "posts" ("lenderName", "gameName", "gameApiId",
+                     "gameThumbNail", "lenderComments")
+values ('Garrett Taylor', 'Catan', 13, 'https://cf.geekdo-images.com/W3Bsga_uLP9kO91gZ7H8yw__thumb/img/8a9HeqFydO7Uun_le9bXWPnidcA=/fit-in/200x150/filters:strip_icc()/pic2419375.jpg',
+        'In great Condition! This is my favorite game.'),
+       ('Steve Rodgers', 'Risk', 1406, 'https://cf.geekdo-images.com/9nGoBZ0MRbi6rdH47sj2Qg__thumb/img/ezXcyEsHhS9iRxmuGe8SmiLLXlM=/fit-in/200x150/filters:strip_icc()/pic5786795.jpg',
+        'This game is ice cold'),
+       ('Remus Lupin', 'One Night Ultimate Werewolf', 147949, 'https://cf.geekdo-images.com/KLDb0vR3w8mfaHgIGF0gHw__thumb/img/ZgQsHsvdxXZr4DbMGUBSTArBp3o=/fit-in/200x150/filters:strip_icc()/pic1809823.jpg',
+        'Love to lay this game with a ton of people on a full moon!');
