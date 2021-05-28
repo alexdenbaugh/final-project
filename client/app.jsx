f3ddbd56098e32384835cdfc32d0ed5c84c34de7
Alex Denbaugh <alexdenbaugh@gmail.com>
2021-05-27 17:49:31 -0700

    Create component to update dom with correct view, Create compoonent with a form for 'New Posts'. #1

--- client/app.jsx
+++ client/app.jsx
@@ -1,37 +1,36 @@
 import React from 'react';
-import Home from './pages/home';
 import Header from './components/header';
 import PageContainer from './components/page-container';
 import NewPostForm from './components/new-post';
-import GameSearch from './components/game-search';
+import AppContext from './lib/app-context';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      change: null
     };
   }
 
   renderPage() {
     return (
       <>
-        <Home />
         <NewPostForm />
-        <GameSearch />
       </>
     );
   }
 
   render() {
+    const contextValue = {};
     return (
       <>
         <Header />
-        <main className="">
-          <PageContainer>
-            { this.renderPage() }
-          </PageContainer>
-        </main>
+        <AppContext.Provider value={ contextValue }>
+          <main>
+            <PageContainer>
+              { this.renderPage() }
+            </PageContainer>
+          </main>
+        </AppContext.Provider>
       </>
     );
   }
--- client/components/new-post.jsx
+++ client/components/new-post.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
 export default class NewPostForm extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searching: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleSearch(event) {
+    // console.log(event.target.value);
+
+  }
+
+  handleClick() {
+    this.setState({ searching: true });
+  }
+
+  renderForm() {
+    let hidden;
+    if (this.state.searching) {
+      hidden = '';
+    } else {
+      hidden = 'hidden';
+    }
     return (
-      <form action="#">
+      <>
         <div className="row">
           <div className="col-4 bookmark title shadow">
             <h1 className="orange">New Post</h1>
@@ -19,7 +42,7 @@ export default class NewPostForm extends React.Component {
                 <label className="orange" htmlFor="new-game-search">Name of Game:</label>
               </div>
               <div className="input">
-                <input required type="text" className="lora shadow" id="new-game-search" placeholder="Search for a game..."/>
+                <input onClick={this.handleClick} required type="text" className="lora shadow" id="new-game-search" placeholder="Search for a game..." />
               </div>
             </div>
             <div className="form-element">
@@ -45,7 +68,39 @@ export default class NewPostForm extends React.Component {
         <div className="row form-button">
           <button className="form-button shadow">Post</button>
         </div>
+        <div className={`modal-container ${hidden}`}>
+          <div className="row">
+            <div className="col-2">
+              <div className="input">
+                <input onKeyUp={this.handleSearch} required type="text" className="lora shadow" id="new-game-search" placeholder="Search for a game..." />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            {/* { SearchList(this.state.searchItems) } */}
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  render() {
+    return (
+      <form action="#" className="game-search" autoComplete="off">
+        { this.renderForm() }
       </form>
     );
   }
 }
+
+// function SearchList(list) {
+//   let searchResults = []
+//   searchResults = list.map(item => {
+//     return (
+//       <div className="col-1">
+//         <h3>This is a game!</h3>
+//       </div>
+//     )
+//   })
+//   return searchResults
+// }
